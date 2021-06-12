cdbd38628c96a77d5d14f0a4a76973f90750d9ff
Aniq Aqil <niqaqil13579@gmail.com>
2021-06-13 00:22:00 +0800

    add MCTS and Tour class

---
+++ Delivery/src/MCTS.java
@@ -0,0 +1,173 @@
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MCTS {
+    private double adjMatrix[][];
+    private Depot d;
+    private static ArrayList<MyCustomer> c;
+    private double distance;
+    //LinkedList<MyCustomer> linkedList = new LinkedList<>();
+    //ArrayList<Vehicle> vehicleList = new ArrayList<>();
+    //StringBuilder sb;
+    //double routeCost;
+    //double tourCost;
+    
+    private int N;
+    private double policy[][][];
+    private double globalPolicy[][];
+    ArrayList<MyCustomer> location;
+    Tour best_tour = new Tour(Double.POSITIVE_INFINITY);
+    int timeLimit = 60;
+    private final int l = 3;
+    private final int ite = 100;
+    
+    public MCTS(ArrayList<MyCustomer> c) {
+        this.c = c;
+        d=(Depot)c.get(0);
+        N = c.size();
+        this.policy = new double[l][N][N];
+        this.globalPolicy = new double[N][N];
+        adjMatrix = new double[c.size()][c.size()];  //store distance between every 2 nodes  customer and depot ,customer and customer
+        //forming the graph
+        for (int i = 0; i < c.size(); i++) { //edge
+            for (int j = 0; j < c.size(); j++) {
+                if(i==j) continue;
+                if(adjMatrix[i][j]!=0)
+                    continue; //because save before
+                distance = Math.sqrt(Math.pow(c.get(i).xCoordinate-c.get(j).xCoordinate,2) + Math.pow(c.get(i).yCoordinate-c.get(j).yCoordinate,2));
+
+                adjMatrix[i][j] = adjMatrix[j][i] = distance;  //because is undirected edge
+            }
+        }
+        this.location = (ArrayList<MyCustomer>) c.clone();
+    }
+    
+    public Tour search(int level, int iteration) {
+        Instant startTime = Instant.now();
+        if(level==0){
+            return rollout();
+        }else{
+            policy[level-1] = globalPolicy;
+            for (int i = 0; i < iteration; i++) {
+                Tour new_tour = search(level-1,iteration);
+                if(new_tour.getTourCost() < best_tour.getTourCost()){
+                    best_tour = new_tour;
+                    adapt(best_tour,level);
+                }
+                if(Duration.between(startTime, Instant.now()).getSeconds() > timeLimit);
+            }
+            globalPolicy = policy[level-1];
+        }
+        return best_tour;
+    }
+    
+    public void adapt(Tour a_tour, int level) {
+        ArrayList<MyCustomer> visited = new ArrayList<>();
+        //for every route in tour
+        for (int i = 0; i < a_tour.getRouteSize(); i++) {
+            for (int j = 0; j < a_tour.getRoute().get(i).size()-1; j++) {
+                int ALPHA = 1;
+                policy[level-1][a_tour.getRoute().get(i).get(j).ID][a_tour.getRoute().get(i).get(j+1).ID] += ALPHA;
+                double z = 0.0;
+                //for every possible move that can be made by stop
+                for (int k = 0; k < location.size(); k++) {
+                    if(location.get(k).ID!=a_tour.getRoute().get(i).get(j).ID){
+                        if(!visited.contains(location.get(k))){
+                            z+= Math.exp(globalPolicy[a_tour.getRoute().get(i).get(j).ID][location.get(k).ID]);
+                        }
+                    }
+                }
+                //for every possible move that can be made by stop
+                for (int k = 0; k < location.size(); k++) {
+                    if(location.get(k).ID != a_tour.getRoute().get(i).get(j).ID){
+                        if(!visited.contains(location.get(k))){
+                            policy[level - 1][a_tour.getRoute().get(i).get(j).ID][location.get(k).ID] -= ALPHA * (Math.exp(globalPolicy[a_tour.getRoute().get(i).get(j).ID][location.get(k).ID]) / z);
+                        }
+                    }
+                }
+                visited.add(a_tour.getRoute().get(i).get(j));
+            }
+        }
+    }
+    
+    public Tour rollout() {
+        MyCustomer currentStop;
+        MyCustomer nextStop;
+
+        ArrayList<MyCustomer> possible_successors = (ArrayList<MyCustomer>)location.clone();
+        possible_successors.remove(0); //depot removed
+
+        ArrayList<MyCustomer> visited = new ArrayList<>();
+        ArrayList<MyCustomer> checked = new ArrayList<>();
+
+        Tour new_tour = new Tour(adjMatrix,c);
+        new_tour.addNewRoute();
+
+        int currentLoad = 0;
+
+        while(true){
+            currentStop = new_tour.getLastStop();
+            for (int i = 0; i < possible_successors.size(); i++) {
+                if(checked.contains(possible_successors.get(i)) || visited.contains(possible_successors.get(i))){
+                    possible_successors.remove(i);
+                }
+            }
+            //if no possible successor is available, return to depot
+            if(possible_successors.isEmpty()){
+                new_tour.addDepot(); //add depot
+                //setRouteCost;
+                //if all stops are visited
+                if(checked.isEmpty()) {
+                    //user for loop to compute Tour cost;
+                    //new_tour.computeRouteCost(linkedList);
+                    break; //rollout completed
+                }
+                //add new route into new tour
+                new_tour.addNewRoute();
+                currentLoad = 0;
+
+                for (int i = 0; i < checked.size(); i++) {
+                    possible_successors.add(checked.remove(i));
+                }
+                continue;      //skip to next loop to continue
+            }
+            nextStop = select_next_move(currentStop,possible_successors);
+
+            if(currentLoad+nextStop.demandSize<=d.maximumCapacity){
+                new_tour.addStop(nextStop);
+                currentLoad += nextStop.demandSize;
+                visited.add(nextStop);
+            }else{
+                checked.add(nextStop);
+            }
+        }
+        return new_tour;
+    }
+    
+    public MyCustomer select_next_move(MyCustomer currentStop, ArrayList<MyCustomer> possible_successors) {
+        double[] probability = new double[possible_successors.size()];
+        double sum = 0;
+        for (int i = 0; i < possible_successors.size(); i++) {
+            probability[i] = Math.exp(globalPolicy[currentStop.ID][possible_successors.get(i).ID]);
+            sum+=probability[i];
+        }
+        double mRand = new Random().nextDouble() * sum;
+        int j = 0;
+        sum = probability[0];
+        while(sum<mRand){
+            sum+=probability[++j];
+        }
+        return possible_successors.get(j);
+    }
+    
+    public void displayTour() {
+        System.out.println(search(l, ite));
+    }
+
+    @Override
+    public String toString() {
+        return best_tour.toString();
+    }
+}
---
+++ Delivery/src/Tour.java
@@ -0,0 +1,108 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Tour {
+    private double tourCost;
+    private ArrayList<LinkedList<MyCustomer>> route = new ArrayList<>();
+    double[][] adjMatrix;
+    List<MyCustomer> cus;
+
+    public Tour(double tourCost) {
+        this.tourCost = tourCost;
+    }
+
+    public Tour(double[][] adjMatrix, List<MyCustomer> cus) {
+        this.adjMatrix = adjMatrix;
+        this.cus = cus;
+        tourCost = 0;
+    }
+
+    public void add(LinkedList<MyCustomer> r) {
+        route.add(r);
+    }
+
+    public ArrayList<LinkedList<MyCustomer>> getRoute() {
+        return route;
+    }
+
+    public double computeTourCost() {
+        tourCost = 0;
+        for (int i = 0; i < route.size(); i++)
+            for (int j = 0; j < route.get(i).size()-1; j++) {
+                int firstPoint = route.get(i).get(j).ID;
+                int secondPoint = route.get(i).get(j+1).ID;
+                tourCost += adjMatrix[firstPoint][secondPoint];
+            }
+        return tourCost;
+    }
+
+    public double computeRouteCost(LinkedList<MyCustomer> c) {
+        double routeCost = 0;
+        int i;
+        for (i = 0; i < c.size()-1; i++) {
+            int firstPoint = c.get(i).ID;
+            int secondPoint = c.get(i+1).ID;
+            routeCost += adjMatrix[firstPoint][secondPoint];
+        }
+        return routeCost;
+    }
+
+    public void addStop(MyCustomer n) {
+        route.get(route.size()-1).add(n);
+        tourCost = computeTourCost();
+    }
+
+    public void addNewRoute() {
+        LinkedList<MyCustomer> tempList = new LinkedList<>();
+        tempList.add(cus.get(0));
+        route.add(tempList);
+        tourCost = computeTourCost();
+    }
+
+    public MyCustomer getLastStop() {
+        int routeSize = route.size()-1;
+        int linkedListRouteSize = route.get(routeSize).size()-1;
+        return route.get(routeSize).get(linkedListRouteSize);
+    }
+
+    public int computeCapacity(LinkedList<MyCustomer> c) {
+        int capacity = 0;
+        for (int i = 1; i < c.size()-1; i++)
+            capacity += c.get(i).demandSize;
+        return capacity;
+    }
+
+    public double getTourCost() {
+        return tourCost;
+    }
+
+    public void setTourCost(double tourCost) {
+        this.tourCost = tourCost;
+    }
+
+    public int getRouteSize() {
+        return route.size();
+    }
+
+    public void addDepot() {
+        route.get(getRouteSize()-1).add(cus.get(0));
+        tourCost = computeTourCost();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("~MCTS Simulation~\nTour Cost: " + tourCost);
+        for (int i = 0; i < route.size(); i++) {
+            sb.append("\nVehicle " + (i+1) + "\n");
+            for (int j = 0; j < route.get(i).size()-1; j++) {
+                sb.append(route.get(i).get(j).ID + " -> ");
+            }
+            sb.append("0\n");
+            sb.append("Capacity: " + computeCapacity(route.get(i)) +"\nCost: " + computeRouteCost(route.get(i)));
+        }
+        return sb.toString();
+    }
+}
